8a1cb8195d68242f166e92056eebfb42e4481990
luthfiamrullah <muhamadluthfiamrullah25@gmail.com>
2024-05-19 22:06:05 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const toRupiah = require('to-rupiah');
+const generateProduct = require('generate-product');
+const numericToolkit = require('numeric-toolkit');
+const calculusToolkit = require('calculus-toolkit');
+const advanceCalculusToolkit = require('advance-calculus-toolkit');
+
+console.log('This is package xenacious-copper-shark');
+console.log(toRupiah(1000));
+console.log(generateProduct());
+console.log(numericToolkit.someFunction());  // Ganti dengan fungsi yang sesuai dari package
+console.log(calculusToolkit.someFunction());  // Ganti dengan fungsi yang sesuai dari package
+console.log(advanceCalculusToolkit.someFunction());  // Ganti dengan fungsi yang sesuai dari package
